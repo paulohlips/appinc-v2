8898c563156416bfbedd4437926ca06390ba9385
Unknown <ayrtonlaceda01@gmail.com>
2019-03-24 17:36:09 -0300

    fix: req security

--- src/routes.js
+++ src/routes.js
@@ -32,7 +32,7 @@ import StepList from './pages/StepList';
 
 const Routes = StackNavigator(
   {
-    Login: { screen: Login },
+    Login: { screen: New },
     SignUp: { screen: Singup },
     Hash: { screen: Hash },
     Password: { screen: Password },

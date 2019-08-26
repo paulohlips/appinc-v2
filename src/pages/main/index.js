8ab0db242269d6f0d429e3d5aae74550db80c026
Paulo Henrique Ferreira <paulohfcmendes@gmail.com>
2019-08-26 12:40:46 -0300

    fix: aapt2

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,44 +1,52 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StatusBar, Image, AsyncStorage, BackHandler } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import styles from './styles';
-import axios from 'axios';
-import { Header } from '../../globalComponents';
-import { Sketch } from '../../components';
-import { responsividade } from '../../styles';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StatusBar,
+  Image,
+  AsyncStorage,
+  BackHandler
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import styles from "./styles";
+import axios from "axios";
+import { Header } from "../../globalComponents";
+import { Sketch } from "../../components";
+import { responsividade } from "../../styles";
+import { connect } from "react-redux";
 
-import { NavigationActions, withNavigation, StackActions } from 'react-navigation';
-import { bindActionCreators } from 'redux';
-import { Creators as UserActions } from '../../store/ducks/login';
-
+import {
+  NavigationActions,
+  withNavigation,
+  StackActions
+} from "react-navigation";
+import { bindActionCreators } from "redux";
+import { Creators as UserActions } from "../../store/ducks/login";
 
 class Main extends Component {
-
-  navigateToScreen = (route) => () => {
+  navigateToScreen = route => () => {
     const navigateAction = NavigationActions.navigate({
       routeName: route
     });
     this.props.navigation.dispatch(navigateAction);
-  }
+  };
 
   static navigationOptions = {
-    header: null,
-  }
+    header: null
+  };
 
   state = {
-    nome: '',
+    nome: "",
     day: 0,
-    drawerStatus: null,
-  }
-  
+    drawerStatus: null
+  };
+
   componentWillMount() {
     const { login, getExitLogin } = this.props;
-
     let days;
     const currentDate = new Date();
-    const date =  new Date(login.valtoken.replace(' ','T'));
-   
+    const date = new Date(login.valtoken.replace(" ", "T"));
 
     const currentDay = currentDate.getDate();
     const currentMonth = currentDate.getMonth();
@@ -48,7 +56,7 @@ class Main extends Component {
 
     if (dateMonth > currentMonth) {
       if (dateDay < currentDay) {
-        days = (30 - currentDay) + dateDay;
+        days = 30 - currentDay + dateDay;
       } else {
         days = 30;
       }
@@ -59,7 +67,7 @@ class Main extends Component {
       getExitLogin();
       this.navigateToLogout();
     } else {
-      this.setState({ day: days })
+      this.setState({ day: days });
     }
   }
 
@@ -68,31 +76,35 @@ class Main extends Component {
       index: 0,
       actions: [
         // Logged
-        NavigationActions.navigate({ routeName: 'Login' }),
+        NavigationActions.navigate({ routeName: "Login" })
       ]
     });
     this.props.navigation.dispatch(resetAction);
-  }
+  };
 
   convertDate = starttime => {
-    // Your default date object  
+    // Your default date object
     //var starttime = new Date();
     // Get the iso time (GMT 0 == UTC 0)
-    var isotime = new Date((new Date(starttime)).toISOString() );    
-    var fixedtime = new Date(isotime.getTime()-(starttime.getTimezoneOffset()*60000));    
-    var formatedMysqlString = fixedtime.toISOString().slice(0, 19).replace('T', ' ');
-    console.log( formatedMysqlString );
+    var isotime = new Date(new Date(starttime).toISOString());
+    var fixedtime = new Date(
+      isotime.getTime() - starttime.getTimezoneOffset() * 60000
+    );
+    var formatedMysqlString = fixedtime
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " ");
+    console.log(formatedMysqlString);
 
     return formatedMysqlString;
-  }
+  };
 
   componentDidMount() {
-    BackHandler.addEventListener('hardwareBackPress', this.navigateToLogin);
+    BackHandler.addEventListener("hardwareBackPress", this.navigateToLogin);
   }
 
   componentWillUnmount() {
-    BackHandler.removeEventListener('hardwareBackPress');
-   
+    BackHandler.removeEventListener("hardwareBackPress");
   }
 
   openDrawer = () => {
@@ -100,23 +112,23 @@ class Main extends Component {
 
     if (drawerStatus === true) {
     }
-  }
+  };
 
   navigateToLogin = async () => {
     const resetAction = StackActions.reset({
       index: 0,
       actions: [
         // Logged
-        NavigationActions.navigate({ routeName: 'Login' }),
+        NavigationActions.navigate({ routeName: "Login" })
       ]
     });
     this.props.navigation.dispatch(resetAction);
-  }
+  };
 
   render() {
-    const { navigation , login } = this.props;
-    const { nome, day } = this.state
-    const name = navigation.getParam('nome', 'Nome não cadastrado');
+    const { navigation, login } = this.props;
+    const { nome, day } = this.state;
+    const name = navigation.getParam("nome", "Nome não cadastrado");
     const { largura_tela } = responsividade;
     return (
       <View style={styles.container}>
@@ -124,7 +136,7 @@ class Main extends Component {
           showMenu
           showExit
           openMenu={navigation.toggleDrawer}
-          title='Inicial'
+          title="Inicial"
         />
 
         <View style={styles.bodyS}>
@@ -136,19 +148,22 @@ class Main extends Component {
 
           <View style={styles.info}>
             <View style={styles.profile}>
-              <Image source={require('../../assents/imgs/avatar.png')} style={styles.ImageStyle} />
+              <Image
+                source={require("../../assents/imgs/avatar.png")}
+                style={styles.ImageStyle}
+              />
             </View>
             <View style={styles.name_view}>
               <Text style={styles.name}>{login.userName}</Text>
             </View>
           </View>
           <View style={styles.buttons_view}>
-            <TouchableOpacity onPress={this.navigateToScreen('NewMenu')}>
+            <TouchableOpacity onPress={this.navigateToScreen("NewMenu")}>
               <View style={styles.button}>
                 <Text style={styles.button_text}>NOVA PERÍCIA</Text>
               </View>
             </TouchableOpacity>
-            <TouchableOpacity onPress={this.navigateToScreen('Hist')}>
+            <TouchableOpacity onPress={this.navigateToScreen("Hist")}>
               <View style={styles.button}>
                 <Text style={styles.button_text}>MINHAS PERÍCIAS</Text>
               </View>
@@ -161,9 +176,13 @@ class Main extends Component {
 }
 
 const mapStateToProps = state => ({
-  login: state.loginState,
+  login: state.loginState
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators(UserActions, dispatch)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(UserActions, dispatch);
 
-export default connect( mapStateToProps , mapDispatchToProps )(Main);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Main);
